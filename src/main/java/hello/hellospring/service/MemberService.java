0c69b76f63b532e39261a3ea00321cf7f59c603f
Jonghyun99 <kstu70015139@gmail.com>
2021-05-09 18:38:29 +0900

    AOP(메소드가 실행되는 시간 구하기)

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,8 +1,10 @@
 package hello.hellospring.service;
 
+import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Optional;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
@@ -10,42 +12,44 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class MemberService {
-	
-	private final MemberRepository memberRepository;
 
-	public MemberService(MemberRepository memberRepository) {
-		this.memberRepository = memberRepository;
-	}
+    private final MemberRepository memberRepository;
 
-	/**
-	 * 회원 가입
-	 * @param member
-	 * @return
-	 */
-	public Long join(Member member) {
-		ValidateDuplicationMember(member); // 중복 회원 검증
-		memberRepository.save(member);
-		return member.getId();
-	}
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
-	private void ValidateDuplicationMember(Member member) {
-		memberRepository.findByName(member.getName())
-			.ifPresent(m -> {
-				throw new IllegalStateException("이미 존재하는 회원 입니다.");
-			});
-	}
-	
-	
-	/**
-	 * 전체 회원 조회
-	 * @return
-	 */
-	public List<Member> findMembers() {
-		return memberRepository.findAll();
-	}
-	
-	public Optional<Member> findOne(Long memberId) {
-		return memberRepository.findById(memberId);
-	}
+    /**
+     * 회원 가입
+     *
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
 
-}
+            ValidateDuplicationMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+    }
+
+    private void ValidateDuplicationMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원 입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     *
+     * @return
+     */
+    public List<Member> findMembers() {
+            return memberRepository.findAll();
+    }
+
+        public Optional<Member> findOne (Long memberId){
+            return memberRepository.findById(memberId);
+        }
+
+    }
