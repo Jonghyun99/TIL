3678bf18ea821fc9edca86df86a5e8b160bfd3b1
jhseo <kstu70015139@gmail.com>
2021-09-01 08:50:54 +0900

    calculator

--- python/practice/calculator.py
+++ python/practice/calculator.py
@@ -1,42 +1,46 @@
-def add(a, b):
-    return a+b
+class Calculator:
+    param1 = None
+    def init(self, first):
+        self.first=first
+        return
 
-def subtract(a,b):
-    return a-b
+    def start(self):
 
-def multiple(a,b):
-    return a*b
+        while True:
+            prompt = """
+            숫자 입력
 
-def div(a,b):
-    return a/b
-while True:
-    prompt = """
-    숫자 입력
+            1. add
+            2. subtract
+            3. multiple
+            4. div
 
-    1. add
-    2. subtract
-    3. multiple
-    4. div
+            input: 
+            """
+            print(prompt)
+            par1 = input()
 
-    input: 
-    """
-    print(prompt)
-    par1 = input()
+            a = input("첫 번째 숫자: ")
+            b = input("두 번째 숫자: ")
+            a=int(a)
+            b=int(b)
 
-    a = input("첫 번째 숫자: ")
-    b = input("두 번째 숫자: ")
-    a=int(a)
-    b=int(b)
+            if par1 == "1":
+                print(a + b)
+            elif par1 == "2":
+                print(a - b)
+            elif par1 == "3":
+                print(a * b)
+            elif par1 == "4":
+                print(a / b)
 
-    if par1 == "1":
-        print(add(a, b))
-    elif par1 == "2":
-        print(subtract(a,b))
-    elif par1 == "3":
-        print(multiple(a,b))
-    elif par1 == "4":
-        print(div(a,b))
-
-    last = input("종료하려면 q 입력")
-    if last == "q":
-        break
+            last = input("종료하려면 q 입력")
+            if last == "q":
+                break
+cal1 = Calculator()
+cal1.init(3)
+# cal1.start()
+cal1.first=10
+print(cal1.first)
+cal1.param1=30
+print(cal1.param1)
