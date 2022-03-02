205c743c29be3ff05957b770ff50b7a2f6bad21f
jhseo <kstu70015139@gmail.com>
2022-03-02 18:04:12 +0900

    MethodReference

--- Stream/src/Optional.java
+++ Stream/src/Optional.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.OptionalInt;
+import java.util.function.Consumer;
 
 public class Optional {
 
@@ -13,12 +14,12 @@ public class Optional {
 
 //        java.util.Optional<String> opt = java.util.Optional.ofNullable("r");
 //        java.util.Optional<Integer> opt = java.util.Optional.ofNullable(integer);
-        java.util.Optional<String> opt = java.util.Optional.empty();
+          java.util.Optional<String> opt = java.util.Optional.empty();
 
 //        System.out.println(opt.getClass());
-//        System.out.println(opt.orElse("NULL!"));
-//        System.out.println("1: " + opt.orElseGet(String::new));
-        System.out.println(opt.isPresent());
+          System.out.println(opt.orElse("NULL!"));
+//        System.out.println(opt.orElseGet(() -> "aa"));
+//        System.out.println(opt.isPresent());
 
 //        java.util.Optional opt2 = java.util.Optional.ofNullable(map);
 //        System.out.println(opt2);
