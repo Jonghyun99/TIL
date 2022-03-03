c9cd22d788a01e13e0818d5d8f8ff1b715230b73
jhseo <kstu70015139@gmail.com>
2022-03-03 11:22:23 +0900

    methodReference

--- Stream/src/Main.java
+++ Stream/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -8,11 +9,18 @@ public class Main {
     static MethodReference obj = new MethodReference();
 
 //    static Consumer func = (a) -> obj.callText("str");
-    static Consumer<Double> func2 = obj::callText;
+//    static Consumer<String> func2 = obj::callText;
 //    static Function func2 = obj::callText;
 
     public static void main(String[] args) {
-        func2.accept(12D);
-
+//        List<String> companies = Arrays.asList("google", "apple", "google", "apple", "samsung");
+//// 1. lambda expression
+//        companies.stream().forEach(company -> System.out.println(company));
+//// 2. static method reference
+//        companies.stream().forEach(System.out::println);
+//
+//        func2.accept("test");
     }
+
+
 }
