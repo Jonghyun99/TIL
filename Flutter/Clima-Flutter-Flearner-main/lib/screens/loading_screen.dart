a0408d76668a85e2435e7174359b78e1bbe09b62
Jonghyun99 <kstu70015139@gmail.com>
2022-03-31 22:36:33 +0900

    Flutter-Async Challenge And About Api

--- Flutter/Clima-Flutter-Flearner-main/lib/screens/loading_screen.dart
+++ Flutter/Clima-Flutter-Flearner-main/lib/screens/loading_screen.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
+import '../services/location.dart';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    try {
-      LocationPermission permission = await Geolocator.requestPermission();
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      print(position);
-    } catch (e){
-      print(e);
-    }
-
-  }
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getLocation();
+    print('this line of code is triggered');
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+
   }
 
   @override
