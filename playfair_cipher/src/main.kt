f0c52ebfb27c5083ee3cecfe24b05c51e9d00163
jhseo <kstu70015139@gmail.com>
2022-05-10 17:33:28 +0900

    cipherText -> 암호키 생성메소드 작성

--- playfair_cipher/src/main.kt
+++ playfair_cipher/src/main.kt
@@ -1,12 +1,26 @@
 package com.main
 
 import KeyTable
-import PlayfairAlgorithm
+import java.util.*
 
 fun main() {
-    var cipherKey:String = "TEST"
+    var plainText:String="" // 평문
+    var cipherText:String="APPLE"
+    var cipherKey = makeCipherKey(cipherText.uppercase(Locale.getDefault()))  //암호문
     var keyTable:KeyTable = KeyTable()
-//    println(keyTable.makeKeyTable("ILIKEAPPLE").contentDeepToString())
-    keyTable.makeKeyTable(cipherKey)
+
+    var key = keyTable.makeKey(cipherKey)
+
+    println("resultKey:${key.contentDeepToString()}")
+}
+
+fun makeCipherKey(plainText:String):String{
+    var cipherKey:String=""
+    for(i in plainText.indices){
+        if(plainText.indexOf(plainText.elementAt(i))==i){
+            cipherKey+=plainText.elementAt(i)
+        }
+    }
+    return cipherKey
 }
 
