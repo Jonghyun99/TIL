6fa8910db83b7140cc0b74dd57017f441f3dfd14
jhseo <kstu70015139@gmail.com>
2022-05-11 18:08:21 +0900

    encryptText Method

--- playfair_cipher/src/PlayfairAlgorithm.kt
+++ playfair_cipher/src/PlayfairAlgorithm.kt
@@ -5,7 +5,10 @@ class PlayfairAlgorithm {
         if(plainText.length%2==1){
             plainText += "X"
         }
+        var encryptText = arrayListOf<Char>() // 암호화 결과 담을 변수
+        var cnt = 0 // for문 2개씩 끊기 위한 flag 변수
+        for(i in plainText){
 
-
+        }
     }
 }
