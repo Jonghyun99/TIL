724533a92954771131535b47a8a42a14bf5b0417
Jonghyun99 <kstu70015139@gmail.com>
2022-05-17 23:39:23 +0900

    평문문자리스트 생성

--- playfair_cipher/src/PlayfairAlgorithm.kt
+++ playfair_cipher/src/PlayfairAlgorithm.kt
@@ -1,14 +1,30 @@
 class PlayfairAlgorithm {
 
     fun encrypt(plainText:String, key:Array<Array<Char?>>){
-        var plainText= ""
-        if(plainText.length%2==1){
-            plainText += "X"
-        }
         var encryptText = arrayListOf<Char>() // 암호화 결과 담을 변수
-        var cnt = 0 // for문 2개씩 끊기 위한 flag 변수
-        for(i in plainText){
 
+        var plainTextList:ArrayList<ArrayList<Char>> = ArrayList<ArrayList<Char>>()
+
+        var flag = 0 // for문 2개씩 끊기 위한 flag 변수
+        var cnt = 0   //for 카운터 변수
+        for(i in plainText.uppercase()){
+            //TODO Index 5 out of bounds for length 5
+            //배열 생성 패턴 수정 (필요한만큼 늘려야함)
+            plainTextList.add(ArrayList())
+            plainTextList[cnt].add(i)
+            flag++
+            println("planTextList = $plainTextList")
+            if(flag>=2){
+                flag=0
+                cnt++
+            }
         }
+
+        // 마지막 자리 부족할 시 X 추가
+        if(plainTextList[plainTextList.size].size<=1){
+            plainTextList[plainTextList.size].add('X')
+        }
+
+        println(plainTextList.toString())
     }
 }
--- playfair_cipher/src/main.kt
+++ playfair_cipher/src/main.kt
@@ -1,19 +1,29 @@
 package com.main
 
 import KeyTable
+import PlayfairAlgorithm
 import java.util.*
 
 fun main() {
-    var plainText:String="" // 평문
-    var cipherText:String="APPLE"
-    var cipherKey = makeCipherKey(cipherText.uppercase(Locale.getDefault()))  //암호문
-    var keyTable:KeyTable = KeyTable()
 
-    var key = keyTable.makeKey(cipherKey)
+    //클래스선언
+    var keyTable:KeyTable = KeyTable()
+    var cipher:PlayfairAlgorithm = PlayfairAlgorithm()
+
+    //변수선언
+    var plainText:String="Hello" // 평문
+    var cipherText:String="APPLE" // 암호문자
+    var cipherKey = makeCipherKey(cipherText.uppercase(Locale.getDefault()))  //암호문 변형 APPLE -> APLE
+
+
+    var key = keyTable.makeKey(cipherKey)   // 암호표
 
     println("resultKey:${key.contentDeepToString()}")
+    cipher.encrypt(plainText,key)
 }
 
+
+//암호키 만드는 함수 ex) APPLE -> APLE
 fun makeCipherKey(plainText:String):String{
     var cipherKey:String=""
     for(i in plainText.indices){
