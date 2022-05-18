dcba255bd8e9051516feb3d353799d829e0cf99a
Jonghyun99 <kstu70015139@gmail.com>
2022-05-18 23:38:24 +0900

    알고리즘 문자리스트 함수 작성

--- playfair_cipher/src/PlayfairAlgorithm.kt
+++ playfair_cipher/src/PlayfairAlgorithm.kt
@@ -3,28 +3,34 @@ class PlayfairAlgorithm {
     fun encrypt(plainText:String, key:Array<Array<Char?>>){
         var encryptText = arrayListOf<Char>() // 암호화 결과 담을 변수
 
-        var plainTextList:ArrayList<ArrayList<Char>> = ArrayList<ArrayList<Char>>()
+        makeTextArray(plainText)    // 텍스트 알고리즘 처리를 위한7 ㅡ, 배열화
+    }
+
+    // HELLO -> [[H,E],[L,X],[O,X]] 암,복호화 알고리즘에 사용되는 텍스트 배열 생성
+    private fun makeTextArray(plainText: String): ArrayList<ArrayList<Char>> {
+        var plainTextList: ArrayList<ArrayList<Char>> = ArrayList<ArrayList<Char>>()
+        plainTextList.add(ArrayList())
 
         var flag = 0 // for문 2개씩 끊기 위한 flag 변수
         var cnt = 0   //for 카운터 변수
-        for(i in plainText.uppercase()){
-            //TODO Index 5 out of bounds for length 5
-            //배열 생성 패턴 수정 (필요한만큼 늘려야함)
-            plainTextList.add(ArrayList())
-            plainTextList[cnt].add(i)
-            flag++
-            println("planTextList = $plainTextList")
-            if(flag>=2){
-                flag=0
+        for (i in plainText.uppercase()) {
+            if (flag >= 2) {
+                flag = 0
                 cnt++
+                plainTextList.add(ArrayList())
             }
+            if (!plainTextList[cnt].isEmpty() && plainTextList[cnt][0] == i) {   // 같은 문자일 시 X 삽입
+                plainTextList[cnt].add('X')
+            } else {
+                plainTextList[cnt].add(i)
+            }
+            flag++
         }
 
         // 마지막 자리 부족할 시 X 추가
-        if(plainTextList[plainTextList.size].size<=1){
-            plainTextList[plainTextList.size].add('X')
+        if (plainTextList[plainTextList.size - 1].size <= 1) {
+            plainTextList[plainTextList.size - 1].add('X')
         }
-
-        println(plainTextList.toString())
+        return plainTextList
     }
 }
